81b2470c4a70946bb1ea5858e57a4bdbcf647de7
Fabian Circelli <u6409647@anu.edu.au>
2019-12-24 13:27:50 +1100

    better construct X, Y grids

--- src/grids.jl
+++ src/grids.jl
@@ -7,28 +7,25 @@ end
 
 function grid(a1, b1, a2, b2, nx, ny)
 
-  L1 = b1 - a1
-  L2 = b2 - a2
+  Lx = b1 - a1
+  Ly = b2 - a2
 
-  dx = L1/nx
-  dy = L2/ny
+  dx = Lx/nx
+  dy = Ly/ny
 
-  x = range(a1, stop = b1 - dx, length = nx)
-  y = range(a2, stop = b2 - dy, length = ny)
+  x = reshape(Array(range(a1, stop = b1 - dx, length = nx)), (nx, 1))
+  y = reshape(Array(range(a2, stop = b2 - dy, length = ny)), (1, ny))
 
-  k = (2pi/L1)*[0:nx/2; -(nx/2 - 1):-1]
-  l = (2pi/L2)*[0:ny/2; -(ny/2 - 1):-1]
+  k = (2pi/Lx)*[0:nx/2; -(nx/2 - 1):-1]
+  l = (2pi/Ly)*[0:ny/2; -(ny/2 - 1):-1]
 
-  X, Y = zeros(nx, ny), zeros(nx, ny)
+  X, Y = x .+ 0*y, 0*x .+ y
 
   K, L = zeros(nx, ny), zeros(nx, ny)
 
   for i in 1:nx, j in 1:ny
-    X[i,j] = x[i]
-    Y[i,j] = y[j]
-
-    K[i,j] = k[i]
-    L[i,j] = l[j]
+    K[i, j] = k[i]
+    L[i, j] = l[j]
   end
 
   return Grid(X, Y, K, L)
