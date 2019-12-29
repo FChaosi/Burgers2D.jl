0535668e5a4a8e1913f9a4e75d764bd2563b043e
Navid Constantinou <navidcy@gmail.com>
2019-12-30 08:20:11 +1100

    minor enhancements in test_spectralderivetive()

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Burgers2D, Test
 
 #Test for x, y grid-spacing
 function test_gridxdy()
-  Lx, Ly = 200, 4pi
-  nx, ny = 100, 200
+  Lx, Ly = 20.2, 4pi
+  nx, ny = 200, 30
   g = Burgers2D.grid(-Lx/2, Lx/2, -Ly/2, Ly/2, nx, ny)
   dx, dy = Lx/nx, Ly/ny
 
@@ -13,7 +13,7 @@ end
 #Test for x-, y-wavenumber spacing
 function test_wavenumbers()
   Lx, Ly = 2*pi, 3.5*pi
-  nx, ny = 100, 300
+  nx, ny = 20, 30
   g = Burgers2D.grid(-Lx/2, Lx/2, -Ly/2, Ly/2, nx, ny)
   dk, dl = 2*pi/Lx, 2*pi/Ly
 
@@ -22,10 +22,16 @@ end
 
 #Test for spectral derivative function
 function test_spectralderivative()
-  g = Burgers2D.grid(-2pi, 2pi, -4pi, 4pi, 100, 300)
-  u = @. sin(g.X)*cos(g.Y)
-  uxx_analytic = @. -sin(g.X)*cos(g.Y)
+  Lx, Ly = 4pi, 7pi
+  g = Burgers2D.grid(-2pi, 2pi, -4pi, 4pi, 40, 60)
+  
+  x, y = g.X, g.Y
+  k0, l0 = 2pi/Lx, 2pi/Ly
+  
+  u = @. sin(2k0*x)*cos(y)
+  uxx_analytic  = @. -(2k0)^2*u
   uxx_numerical = Burgers2D.specd(u, g.K, 2)
+  
   return isapprox(uxx_analytic, uxx_numerical, rtol=1e-12)
 end
 
